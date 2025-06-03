a07228eb04c20537cb18185125ab011a9944241f
Phuc <phucgolkk02@gmail.com>
2025-06-03 21:56:49 +0700

    new code

--- bin/nodejs123.js
+++ bin/nodejs123.js
@@ -4,18 +4,7 @@ const cdk = require('aws-cdk-lib');
 const { WebAppStack } = require('../lib/nodejs123-stack');
 
 const app = new cdk.App();
-new WebAppStack(app, 'Nodejs123Stack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const envName = app.node.tryGetContext("env") || "dev"; // mặc định là dev
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+new WebAppStack(app, 'Nodejs123Stack-${envName}', {
+envName});
