659013970bc4cb6bba642bda589bdcc6c9a0f9b6
Phuc <phucgolkk02@gmail.com>
2025-06-04 09:03:16 +0700

    fix error stack

--- bin/nodejs123.js
+++ bin/nodejs123.js
@@ -6,5 +6,6 @@ const { WebAppStack } = require('../lib/nodejs123-stack');
 const app = new cdk.App();
 const envName = app.node.tryGetContext("env") || "dev"; // mặc định là dev
 
-new WebAppStack(app, 'Nodejs123Stack-${envName}', {
-envName});
+new WebAppStack(app, `Nodejs123Stack-${envName}`, {
+  envName
+});
